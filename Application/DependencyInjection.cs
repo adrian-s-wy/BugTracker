74b70a43656f8e279b860f2d326b401073351154
Adrian Sadowy <adrian.sadowy@gmail.com>
2021-03-07 17:50:18 +0100

    [feat] Add Application project

---
+++ Application/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MediatR;
+using AutoMapper;
+using System;
+using System.Reflection;
+
+namespace Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            return services;
+        }
+    }
+}
