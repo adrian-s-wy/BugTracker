c96927c83c57787196099823b72154904965f861
Adrian Sadowy <adrian.sadowy@gmail.com>
2021-03-07 18:06:24 +0100

    [feat] Add application db context

---
+++ Application/Common/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Common.Interfaces
+{
+    public interface IApplicationDbContext
+    {
+        DbSet<User> Users { get; set; }
+        DbSet<Bug> Bugs { get; set; }
+        DbSet<DomainTask> Tasks { get; set; }
+        DbSet<Note> Notes { get; set; }
+        DbSet<Priority> Priorities { get; set; }
+        DbSet<Project> Projects { get; set; }
+        DbSet<ProjectGroup> ProjectGroups { get; set; }
+        DbSet<ProjectVersion> ProjectVersions { get; set; }
+        DbSet<Status> Statuses { get; set; }
+        DbSet<UserGroup> UserGroups { get; set; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
--- Infrastructure/Persistence/ApplicationDbContext.cs
+++ Infrastructure/Persistence/ApplicationDbContext.cs
@@ -1,10 +1,27 @@
-﻿using System;
+﻿using Application.Common.Interfaces;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Infrastructure.Persistence
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : DbContext, IApplicationDbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Bug> Bugs { get; set; }
+        public DbSet<DomainTask> Tasks { get; set; }
+        public DbSet<Note> Notes { get; set; }
+        public DbSet<Priority> Priorities { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<ProjectGroup> ProjectGroups { get; set; }
+        public DbSet<ProjectVersion> ProjectVersions { get; set; }
+        public DbSet<Status> Statuses { get; set; }
+        public DbSet<UserGroup> UserGroups { get; set; }
     }
 }
