237da0187305a1d6a4dd8f4ed9ede02e6745da59
Adrian Sadowy <adrian.sadowy@gmail.com>
2021-03-11 19:31:48 +0100

    [feat] Add database connection configuration

---
+++ Infrastructure/DependencyInjection.cs
@@ -0,0 +1,25 @@
+﻿using Application.Common.Interfaces;
+using Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                    options.UseMySql(configuration.GetConnectionString("BugTrackerDatabase"))
+                );
+
+            services.AddTransient<IApplicationDbContext>(provider => provider.GetService<ApplicationDbContext>());
+
+            return services;
+        }
+    }
+}
--- WebApp/Startup.cs
+++ WebApp/Startup.cs
@@ -1,3 +1,4 @@
+using Infrastructure;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -23,6 +24,8 @@ namespace WebApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddInfrastructure(Configuration);
+
             services.AddControllersWithViews();
         }
 
